9a2cb0ff26ef9e311a3581a07be45b7e9f12f542
Radist <daniel.radist@gmail.com>
2019-10-24 20:59:38 +0700

    new class Triangle

---
+++ src/Triangle.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Triangle {
+    private float first;        //катет a
+    private float second;       //катет b
+
+    public void init(float a, float b)
+    {
+        first = a;
+        second = b;
+    }
+
+    public float getFirst()
+    {
+        return first;
+    }
+
+    public float getSecond()
+    {
+        return second;
+    }
+
+    public void display()
+    {
+        System.out.printf("a = %f; b = %f", first, second);
+    }
+
+    public void read()
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a:");
+        float a = in.nextFloat();
+
+        System.out.println("Enter b:");
+        float b = in.nextFloat();
+
+        init(a, b);
+    }
+
+    public float areaTriangle()
+    {
+        return first * second / 2;
+    }
+}
